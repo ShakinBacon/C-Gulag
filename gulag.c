b627b3364f883ac4eab979a7745d0d3242d68a9d
Maksym Yaroshenko <max.yaroshenko8@gmail.com>
2019-04-04 08:53:40 +0000

    file open works

--- gulag.c
+++ gulag.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-main() {
+int main() {
     FILE *fp;
     fp = fopen("input", "r");
     
+    //char contents[100], contents2[10];
     
+    int task;
+    int key;
+    char hash;
     
-    
+    fscanf(fp, "%d %c %d", &task, &hash, &key);
+    printf("%d %c%d\n", task, hash, key);
     
     
     fclose(fp);
