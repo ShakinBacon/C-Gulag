62e4520d256c2bcca61759641db2ba04176f1545
Maksym Yaroshenko <max.yaroshenko8@gmail.com>
2019-04-09 06:54:32 +0000

    differentiates between capital and non capitals, ignores other symbols, black magic powered rotation prototype, breaks when ascii value is should loop back to the smallest letter

--- gulag.c
+++ gulag.c
@@ -2,25 +2,43 @@
 
 int main() {
     FILE *fp;
-    fp = fopen("input", "r+"); //opens input file
-    
+    FILE *fo;
+    fp = fopen("input", "r"); //opens input file
+    fo = fopen("output", "w"); // creates output file?
     //char contents[10];
     //int n = 0;
     int task;
     int key;
     char hash;
     
-    fscanf(fp, "%d %c %d", &task, &hash, &key); //debug code: reading instructions
-    printf("%d %c %d\n", task, hash, key);      //debug code: printing instructions
+    fscanf(fp, "%d %c %d", &task, &hash, &key); //reading instructions/key
+    printf("%d %c %d\n", task, hash, key);      //debug code; printing instructions/key
+    switch (task) { //prints the performed task and key for user clarity
+        case 1: printf("Encrypting with rotation cipher using key %d\n", key); break;
+        case 2: printf("Encrypting with substitution cipher using key %d \n", key); break;
+        case 3: printf("Decrypting rotation cipher text using key %d \n", key); break;
+        case 4: printf("Decrypting substitution cipher text using key %d \n", key); break;
+        default: printf("Unkown command %d\n", task);
+    }
     
-    while (!feof(fp)) { //debug code: printing text block
+    while (!feof(fp)) { //rudamentary rotation cipher prototype
         char tmp[0];
         fscanf(fp, "%c", &tmp[0]);
-        printf("%d\n", tmp[0]);
+        int n = tmp[0];
+        if (n > 65 && n < 90) {
+            tmp [0] = tmp[0] + key;
+            fprintf(fo, "%c", tmp[0]);
+            printf("%c", tmp[0]);
+        } else if (n > 96 && n < 123){
+            tmp [0] = tmp[0] + key;
+            fprintf(fo, "%c", tmp[0]);
+            printf("%c", tmp[0]);
+        } else {
+            fprintf(fo, "%c", tmp[0]);
+            printf("%c", 32);
+        }    
     } 
-
-        
-          
-        
+    
+    
     fclose(fp);
 return 0; }
