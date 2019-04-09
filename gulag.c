6c229cb2c7c3351ac175d2ec7549d5bc7a28a4fa
Maksym Yaroshenko <max.yaroshenko8@gmail.com>
2019-04-09 02:21:31 +0000

    reads text block as ascii

--- gulag.c
+++ gulag.c
@@ -2,17 +2,25 @@
 
 int main() {
     FILE *fp;
-    fp = fopen("input", "r");
+    fp = fopen("input", "r+"); //opens input file
     
-    //char contents[100], contents2[10];
-    
+    //char contents[10];
+    //int n = 0;
     int task;
     int key;
     char hash;
     
-    fscanf(fp, "%d %c %d", &task, &hash, &key);
-    printf("%d %c%d\n", task, hash, key);
+    fscanf(fp, "%d %c %d", &task, &hash, &key); //debug code: reading instructions
+    printf("%d %c %d\n", task, hash, key);      //debug code: printing instructions
     
-    
+    while (!feof(fp)) { //debug code: printing text block
+        char tmp[0];
+        fscanf(fp, "%c", &tmp[0]);
+        printf("%d\n", tmp[0]);
+    } 
+
+        
+          
+        
     fclose(fp);
 return 0; }
