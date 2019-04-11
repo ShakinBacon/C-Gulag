7576ab090b30e774f599ea9795ffbee1dfe87cf9
Maksym Yaroshenko <max.yaroshenko8@gmail.com>
2019-04-11 09:23:45 +0000

    fully functioning substitution cipher encryptor implemented

--- gulag.c
+++ gulag.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     FILE *fp;
@@ -8,14 +7,33 @@ int main() {
     fp = fopen("input", "r");   //opens input file
     fo = fopen("output", "w");  //opens output file
     int task;
-    int key;
-    char hash;
+    int key = 0;
+    char hash = 35;
+    char subKey[25] = {0};
+    //fscanf(fp, "%d %c %d", &task, &hash, &key);        //reading instructions/key
     
-    fscanf(fp, "%d %c %d", &task, &hash, &key); //reading instructions/key
-    printf("%d\n%c%d\n", task, hash, key);      //debug code; printing instructions
-    switch (task) { //detects and prints the selected task and key for user clarity
+    fscanf(fp, "%d", &task);
+    if (task == 1 || task == 3) {
+        fscanf(fp, "%c %d", &hash, &key); 
+    } else if (task == 2 || task == 4) {
+        int count = 0;
+        while (count < 28) {
+            fscanf(fp, "%c", &subKey[count]);
+            count++;
+        }        
+    } else {
+        printf ("why are you like this?");
+    }
+
+    
+    
+    
+    
+    
+    printf("%d %c%d\n", task, hash, key);             //debug code; printing instructions
+    switch (task) {                                    //detects and prints the selected task and key for user clarity
         case 1: printf("Encrypting with rotation cipher using key: %c%d\n", hash, key); 
-            while (!feof(fp)) { //fully functioning rotation cipher (key 0 >>> 25)
+            while (!feof(fp)) {                        //fully functioning rotation cipher (key 0 >>> 25)
                 char tmp[1] = {0};
                 fscanf(fp, "%c", &tmp[0]);
                 int n = tmp[0];
@@ -39,11 +57,24 @@ int main() {
                 printf("%c", tmp[0]);
             } 
             break;
-        case 2: printf("Encrypting with substitution cipher using key: %d\n", key); 
-            
+        case 2: printf("Encrypting with substitution cipher using alphabet key:\n%s\n", subKey);
+            while(!feof(fp)) {                         //fully functioning substitution cipher (Alphabet key must be CAPS)
+                char tmp[1] = {0};
+                fscanf(fp, "%c", & tmp[0]);
+                int n = tmp[0];
+                if (n > 64 && n < 91) {                //checks for capital letters
+                    n = n - 63;
+                    tmp[0] = subKey[n];
+                } else if (n > 96 && n < 123) {        //checks for lower case letters
+                    n = n - 95;
+                    tmp[0] = subKey[n];
+                }
+                fprintf(fo, "%c", tmp[0]);
+                printf("%c", tmp[0]);
+            }      
             break;
         case 3: printf("Decrypting rotation cipher text using key: %d\n", key); 
-            while (!feof(fp)) { //fully functioning rotation cipher decryptor
+            while (!feof(fp)) {                        //fully functioning rotation cipher decryptor
                 char tmp[1] = {0};
                 fscanf(fp, "%c", &tmp[0]);
                 int n = tmp[0];
@@ -67,10 +98,10 @@ int main() {
                 printf("%c", tmp[0]);
             }
             break; 
-        case 4: printf("Decrypting substitution cipher text using key: %d\n", key); 
+        case 4: printf("Decrypting substitution cipher text using alphabet key:\n%s\n", subKey); 
             
             break;
-        default: printf("Unkown command %d\n", task);
+        default: printf("Unkown command %d", task);
     }
     
     
