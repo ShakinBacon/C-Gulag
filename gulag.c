015dce4b33b1be2d0973a07cee6680459d6126b9
Maksym Yaroshenko <max.yaroshenko8@gmail.com>
2019-04-19 06:20:12 +0000

    first iteration of substitution decryptor, the code can succesfully find the most common letter, and replace all those letters with any E, next step is to continue this process for the most common letters in the pattern  "e t a o i n s r h l d c”

--- gulag.c
+++ gulag.c
@@ -32,7 +32,7 @@ int main() {
         }
     }
 
-    
+    int count;
     
     printf("%d %c%d\n", task, hash, key);         //debug code; printing instructions
     switch (task) {                                    //detects and prints the selected task and key for user clarity, and directs the code to the correct encryption/decryption operation
@@ -119,8 +119,8 @@ int main() {
             }
             break;
         case 5: printf("Brute forcing rotation cipher text:\n");
-            int count = 0;
-            CYCLE: while (count < 1000) {                  //counts up to the possible limit of letters in tmp1, basically this makes sure every letter of cipher text is read
+            count = 0;
+            CYCLE: while (count < 1000) {                  //counts up to the possible limit of letters in tmp1, basically this makes sure every letter of cipher text is read, also the CYCLE label is here for a later goto function to use
                 count = 0;
                 while (tmp1[count] != 0) {                 //copy pasted code from case 1 reused to rotate unknown text
                     char tmp[1] = {0};
@@ -157,9 +157,50 @@ int main() {
                 }
             }
             break;
-        case 6: printf("Brute forcing substitution cipher text:\n");
+        case 6: printf("Brute forcing substitution cipher text, this may not be fully accurate:\n");
+            count = 0;
+            int numOfLetters[26] = {0};                    //array to keep track of number of letters in laphabetical order
+            while (tmp1[count] != 0) {                     //counts the numbers of letters in the cipher text and assigns them to appropriate positions in the above array
+                int holdThis = tmp1[count];
+                if (holdThis > 64 && holdThis < 91) {    
+                    holdThis = holdThis - 64;
+                    ++numOfLetters[holdThis];
+                }
+                ++count;
+            }
+            int largest;
+            for (count = 0; count < 25; ++count){          //finds the highest number in the above array
+                if (numOfLetters[count] > numOfLetters[count+1]) {
+                    largest = numOfLetters[count];
+                } else if (numOfLetters[count+1] > numOfLetters[count]) {
+                    largest = numOfLetters[count+1];
+                }
+            }
+            printf("%d\n", largest); //debug highest number
+            for (count = 0; count < 25; ++count){          //deduces which letter there is the most of
+                if (numOfLetters[count] == largest) {
+                    break;
+                }
+            }
+            int letter = count+64;
+            printf("%c\n", letter); //debug what letter
+            count = 0;
+            while (tmp1[count] != 0) {                     //assigns the most common letter the value of 69, ASCII for 'E', assuming this from its commonality in the english language
+                if (tmp1[count] == letter){
+                    tmp1[count] = 69;
+                }
+                ++count;
+            }
             
             
+            
+            
+            
+            
+            
+            
+            
+            printf("%s", tmp1); //debug full text
             break;
         default: printf("Unkown command %d", task);
     }
@@ -168,7 +209,7 @@ int main() {
     
     fclose(fp);                                          //close input file
     fclose(fo);                                          //close output file
-printf("done");
+printf("\n\ndone");         //debug end flag
 return 0; 
 }
 
